dcee820472881f022a3126d12f0b28c30822934e
Stefany Dyulgerova <stefany.dyulgerova@gmail.com>
2016-04-25 14:39:31 -0500

    indentation

--- index.php
+++ index.php
@@ -7,20 +7,20 @@
 		<table id="first_page">
 				<tbody>
 					<?php 
-				foreach ($categories as $key => $category) 
-				{
-					$posts->setCategoryId($category['categories_id']);
-					printf('<tr><th> %s </th> <th class="num_topics"> Topics</th> </tr>',
-							 Links::makeLink(
-							 				$category['categories_id'],
-							 				Links::cleanUrl($category['categories_name']),
-							   				$category['categories_name']
-							   				
-							   			)
-							);
-					printf('<tr><td> %s </td> <td class="count"> %d </td></td></tr>', $category['description'], $posts->countTopics());
-				}
-			?>
+						foreach ($categories as $key => $category) 
+						{
+							$posts->setCategoryId($category['categories_id']);
+							printf('<tr><th> %s </th> <th class="num_topics"> Topics</th> </tr>',
+									 Links::makeLink(
+										 				$category['categories_id'],
+										 				Links::cleanUrl($category['categories_name']),
+										   				$category['categories_name']
+									   				)
+								  );
+							printf('<tr><td> %s </td> <td class="count"> %d </td></td></tr>',
+											 $category['description'], $posts->countTopics());
+						}
+					?>
 			</tbody></table>
 
 <?php include 'html/footer.php'; ?>
