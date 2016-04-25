b6e012478e3d68f65b9c92ab01db40579b0ac01e
Stefany Dyulgerova <stefany.dyulgerova@gmail.com>
2016-04-25 14:20:06 -0500

    removed old code

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 	$categories = $posts->getCategories();
 	$posts->setTable('posts');
 ?>
-		
-	
 		<table id="first_page">
 				<tbody>
 					<?php 
@@ -23,41 +21,6 @@
 					printf('<tr><td> %s </td> <td class="count"> %d </td></td></tr>', $category['description'], $posts->countTopics());
 				}
 			?>
-		<!-- <tr>
-			<th><?php// echo Links::makeLink('1', 'military-history', 'Military History');?></th>
-			
-		</tr>
-		<tr>
-			<td>Discuss battles, rate generals, prove how you 
-									would have won a certain battle.</td>
-			<td class="count">2</td>
-		</tr>
-				<tr>
-			<th><a href="topics.php?category_id=2">Social History</a></th>
-			<th class="num_topics"> Topics</th>
-		</tr>
-		<tr>
-			<td>Marriages of the royalties/aristocrats, the best mistress of Henry VIII, how people lived in the past, 
-					what they cooked, etc.</td>
-			<td class="count">1</td>
-		</tr>
-				<tr>
-			<th><a href="history-books">History Books</a></th>
-			<th class="num_topics"> Topics</th>
-		</tr>
-		<tr>
-			<td>Rate books, recommend/seek recommendation of history books, or write one.</td>
-			<td class="count">1</td>
-		</tr>
-				<tr>
-			<th><a href="off-topic">Off Topic</a></th>
-			<th class="num_topics"> Topics</th>
-		</tr>
-		<tr>
-			<td>Discussions about anything except history.</td>
-			<td class="count">1</td>
-		</tr> -->
-		
 			</tbody></table>
 
 <?php include 'html/footer.php'; ?>
